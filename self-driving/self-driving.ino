166eea36c74c61336c8ba6d1f785737b9f407cda
Mizan <86833227+MizanZero@users.noreply.github.com>
2024-12-04 14:56:34 +0530

    smooth speed control and afmotor library

--- self-driving/self-driving.ino
+++ self-driving/self-driving.ino
@@ -1,156 +1,176 @@
-// Setup Motor A (front and rear) pins
-int ENA = 1;
-int A1 = 3;
-int A2 = 2;
+//ARDUINO OBSTACLE AVOIDING CAR//
+// Before uploading the code you have to install the necessary library//
+//AFMotor Library https://learn.adafruit.com/adafruit-motor-shield/library-install //
+//NewPing Library https://github.com/livetronic/Arduino-NewPing// 
+//Servo Library https://github.com/arduino-libraries/Servo.git //
+// To Install the libraries go to sketch >> Include Library >> Add .ZIP File >> Select the Downloaded ZIP files From the Above links //
 
-// Setup Motor B (front and rear) pins
-int ENB = 6;
-int B1 = 5;
-int B2 = 4;
 
-// Setup Ultrasonic Sensor pins
-#define trigPin 8
-#define echoPin 9
+#include <AFMotor.h>  
+#include <NewPing.h>
+#include <Servo.h> 
+
+#define TRIG_PIN A0 
+#define ECHO_PIN A1 
+#define MAX_DISTANCE 200 
+#define MAX_SPEED 190 // sets speed of DC  motors
+#define MAX_SPEED_OFFSET 20
+
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE); 
+
+AF_DCMotor motor1(1, MOTOR12_1KHZ); 
+AF_DCMotor motor2(2, MOTOR12_1KHZ);
+AF_DCMotor motor3(3, MOTOR34_1KHZ);
+AF_DCMotor motor4(4, MOTOR34_1KHZ);
+Servo myservo;   
+
+boolean goesForward=false;
+int distance = 100;
+int speedSet = 0;
 
 void setup() {
-  // The setup code goes here and runs once only
-  // Configure the pin modes for each drive motor
-   pinMode (enableA, OUTPUT);
-   pinMode (pinA1, OUTPUT);
-   pinMode (pinA2, OUTPUT);
 
-   pinMode (enableB, OUTPUT);
-   pinMode (pinB1, OUTPUT);
-   pinMode (pinB2, OUTPUT);
-
-   // Configure the pin modes for the Ultrasonic Sensor
-   pinMode(trigPin, OUTPUT);
-   pinMode(echoPin, INPUT);
+  myservo.attach(10);  
+  myservo.write(115); 
+  delay(2000);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
 }
 
 void loop() {
-  // Main code goes here and will run repeatedly:
-  car(); // function keeps moving car forward while distance of objects in front are > 15cm away
-  avoid(); // function makes car go back, turn slightly right to move forward in new direction
-}
+ int distanceR = 0;
+ int distanceL =  0;
+ delay(40);
+ 
+ if(distance<=15)
+ {
+  moveStop();
+  delay(100);
+  moveBackward();
+  delay(300);
+  moveStop();
+  delay(200);
+  distanceR = lookRight();
+  delay(200);
+  distanceL = lookLeft();
+  delay(200);
 
-// Create motor functions
-void motorAforward() {
- digitalWrite (pinA1, HIGH);
- digitalWrite (pinA2, LOW);
-}
-void motorBforward() {
- digitalWrite (pinB1, LOW);
- digitalWrite (pinB2, HIGH);
-}
-void motorAbackward() {
- digitalWrite (pinA1, LOW);
- digitalWrite (pinA2, HIGH);
-}
-void motorBbackward() {
- digitalWrite (pinB1, HIGH);
- digitalWrite (pinB2, LOW);
-}
-void motorAstop() {
- digitalWrite (pinA1, HIGH);
- digitalWrite (pinA2, HIGH);
-}
-void motorBstop() {
- digitalWrite (pinB1, HIGH);
- digitalWrite (pinB2, HIGH);
-}
-void motorAcoast() {
- digitalWrite (pinA1, LOW);
- digitalWrite (pinA2, LOW);
-}
-void motorBcoast() {
- digitalWrite (pinB1, LOW);
- digitalWrite (pinB2, LOW);
-}
-void motorAon() {
- digitalWrite (enableA, HIGH);
-}
-void motorBon() {
- digitalWrite (enableB, HIGH);
-}
-void motorAoff() {
- digitalWrite (enableA, LOW);
-}
-void motorBoff() {
- digitalWrite (enableB, LOW);
-}
-
-// Setup movement functions
-void forward (int duration) {
- motorAforward();
- motorBforward();
- delay (duration);
-}
-void backward (int duration) {
- motorAbackward();
- motorBbackward();
- delay (duration);
-}
-void right (int duration) {
- motorAbackward();
- motorBforward();
- delay (duration);
-}
-void left (int duration) {
- motorAforward();
- motorBbackward();
- delay (duration);
-}
-void coast (int duration) {
- motorAcoast();
- motorBcoast();
- delay (duration);
-}
-void breakRobot (int duration) {
- motorAstop();
- motorBstop();
- delay (duration);
-}
-void disableMotors() {
- motorAoff();
- motorBoff();
-}
-void enableMotors() {
- motorAon();
- motorBon();
-}
-
-// Setup Ultrasonic Sensor distance measuring
-int distance() {
-  int duration, distance;
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(1000);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration/2) / 29.1;
-  return distance;
-}
-
-// Setup the main car function
-void car() {
-int distance_0;
-distance_0 = distance();
-  // Keep moving forward in a straight line while distance of objects in front > 15cm away
-  while(distance_0 > 15)
+  if(distanceR>=distanceL)
   {
-     motorAon();
-     motorBon();
-     forward(1);
-     distance_0 = distance();
+    turnRight();
+    moveStop();
+  }else
+  {
+    turnLeft();
+    moveStop();
   }
-  breakRobot(0);
-
+ }else
+ {
+  moveForward();
+ }
+ distance = readPing();
 }
 
-// Go back and turn slightly right to move car in new direction
-// This function only runs if an obstacle within 15cm is detected
-void avoid()
+int lookRight()
 {
-    backward(500);
-    right(360);
+    myservo.write(50); 
+    delay(500);
+    int distance = readPing();
+    delay(100);
+    myservo.write(115); 
+    return distance;
 }
+
+int lookLeft()
+{
+    myservo.write(170); 
+    delay(500);
+    int distance = readPing();
+    delay(100);
+    myservo.write(115); 
+    return distance;
+    delay(100);
+}
+
+int readPing() { 
+  delay(70);
+  int cm = sonar.ping_cm();
+  if(cm==0)
+  {
+    cm = 250;
+  }
+  return cm;
+}
+
+void moveStop() {
+  motor1.run(RELEASE); 
+  motor2.run(RELEASE);
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
+  } 
+  
+void moveForward() {
+
+ if(!goesForward)
+  {
+    goesForward=true;
+    motor1.run(FORWARD);      
+    motor2.run(FORWARD);
+    motor3.run(FORWARD); 
+    motor4.run(FORWARD);     
+   for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) // slowly bring the speed up to avoid loading down the batteries too quickly
+   {
+    motor1.setSpeed(speedSet);
+    motor2.setSpeed(speedSet);
+    motor3.setSpeed(speedSet);
+    motor4.setSpeed(speedSet);
+    delay(5);
+   }
+  }
+}
+
+void moveBackward() {
+    goesForward=false;
+    motor1.run(BACKWARD);      
+    motor2.run(BACKWARD);
+    motor3.run(BACKWARD);
+    motor4.run(BACKWARD);  
+  for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) // slowly bring the speed up to avoid loading down the batteries too quickly
+  {
+    motor1.setSpeed(speedSet);
+    motor2.setSpeed(speedSet);
+    motor3.setSpeed(speedSet);
+    motor4.setSpeed(speedSet);
+    delay(5);
+  }
+}  
+
+void turnRight() {
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(BACKWARD);
+  motor4.run(BACKWARD);     
+  delay(500);
+  motor1.run(FORWARD);      
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);      
+} 
+ 
+void turnLeft() {
+  motor1.run(BACKWARD);     
+  motor2.run(BACKWARD);  
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);   
+  delay(500);
+  motor1.run(FORWARD);     
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
+}  
