020902a0e59b837565e9b32589e22b0e1e583372
MizanZero <mizan.zafar2016@gmail.com>
2024-12-05 08:59:16 +0530

    most of prevent and ir functions

--- self-driving/improved.cpp
+++ self-driving/improved.cpp
@@ -1,22 +1,53 @@
 void loop(){
 
- d=ult.getDist();
- delay(10);
+ ir[]=checkIr();
+ passiveLookout();
+ start(d);
+ ir=checkIr();
+ avoid(ir);
 
- avoid(detectAt(d));
+
 }
 
 
-void avoid(int lvl){
+void avoid(){
  switch(scout()){
-  case "small room":
+  case "preventable":
    prevent();
   case "too close":
    halt();
   case "entered large room":
    reverse(); //already contains halt()
-  case "large room":
+  case "realised large room":
    navigate();
-}}
+}//avoid function
+
+
+int* checkIr(){
+ frontIr=digitalRead(frontIrPin);
+ backIr=digitalRead(backIrPin);
+ leftIr=digitalRead(leftIrPin);
+ rightIr=digitalRead(rightIrPin);
+ ir[]={-1,frontIr,backIr,leftIr,rightIr}
+
+ return ir;
+
+}//Ir function
+
+
+void prevent();
+ srv.write(slightLeft);
+ slDist=ult.getDist();
+ srv.write(slighRight)
+ srDist=ult.getDist();
+
+ if(srDist>=slDist){
+  turn("sr");
+  delay(frontDist/speed);
+  turn("sl"); }
+ else{
+  turn("sl");
+  delay(frontDist/speed);
+  turn("sr");
 
 
