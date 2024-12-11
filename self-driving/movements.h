5a55a45049165f59564dde9e8e562d2f5c3f7c6f
MizanZero <mizan.zafar2016@gmail.com>
2024-12-11 12:52:01 +0530

    l29d update

---
+++ self-driving/movements.h
@@ -0,0 +1,9 @@
+#include <AFMotor.h>
+
+AFMotor motor;
+AF_DCMotor motor1(1, MOTOR12_1KHZ);
+AF_DCMotor motor2(2, MOTOR12_1KHZ);
+AF_DCMotor motor3(3, MOTOR34_1KHZ);
+AF_DCMotor motor4(4, MOTOR34_1KHZ);
+
+void 
---
+++ self-driving/self-driving.cpp
@@ -0,0 +1,182 @@
+
+#include <Servo.h>
+#include <NewPing.h>
+#include <AFMotor.h>
+#include <string.h>
+
+#include <C:/Users/Mizan/Documents/RnD Project/self-driving/movement.h>
+
+Servo srv;
+
+
+bool timerRunning(int startTime,int duration){
+ if(millis()-startTime<duration){
+  return true;
+ }
+ return false;
+}
+
+string scout(int distFront,bool ir=false){//returns the type of blockage
+ if(ir==false){
+  if(distFront>=PREVENTABLE_DISTANCE){
+   return "";
+  }
+  else if(distFront<=PREVENTABLE_DISTANCE && distFront>=LESS_PREVENTABLE_DISTANCE){
+   return "preventable";
+  }
+  else if(distFront<=PREVENTABLE_DISTANCE && distFront>=SAFE_DISTANCE){
+   return "less preventable";
+  }
+  else if(distFront<=SAFE_DISTANCE){
+   return "too close";
+  else if(){
+   return "entered large room";
+  }
+  else if(){
+   return "realised large room/large object";
+  }
+  else if(){
+   return "ir level object";
+  }
+  }
+ }
+}
+ }
+
+
+void loop(){
+ delay(50);
+ if(!irClear()){
+  halt();
+ }
+ distFront=ult.dist();
+ if(distFront<IGNORABLE_DISTANCE){
+  avoid(scout());
+ }
+ moveForward();
+
+}
+
+
+void avoid(string obstructType){
+ switch(obstructType){
+  case "preventable":
+   prevent();
+  case "less preventable":
+   slowPrevent();
+  case "too close":
+   halt();
+  case "entered large room":
+   reverse(); //already contains stop()
+  case "realised large room/large object":
+   navigate();
+  case "ir level object":
+   blindNavigate();
+}//avoid function
+
+
+int* irClear(){
+ frontIr=digitalRead(frontIrPin);
+ backIr=digitalRead(backIrPin);
+ leftIr=digitalRead(leftIrPin);
+ rightIr=digitalRead(rightIrPin);
+ ir[]={-1,frontIr,backIr,leftIr,rightIr}
+
+ if(ir!={-1,0,0,0,0}){
+  return false;
+ }
+
+ return ir;
+
+}//Ir function
+
+
+void prevent(string dir){
+ speed(MIN_SPEED);
+ srv.write(slightLeft);
+ slDist=ult.getDist();
+ srv.write(slighRight)
+ srDist=ult.getDist();
+
+ if(srDist>=slDist){
+  turn("sr");
+  delay(avdSep/speed);
+  turn("sl");
+  position=1 }
+ else{
+  turn("sl");
+  delay(avdSep/speed);
+  turn("sr");
+  position=-1; }
+
+  if(passiveLookout()!=clear){
+   switch(position){
+    case 1: turn("left"); delay(avdSep/speed);
+            turn("right");
+
+    case -1: turn("right"); delay(avdSep/speed);
+	     turn("left"); }/*switch end*/
+   }//if end
+  else{}
+}//end of prevent function
+
+
+void halt(){
+ stop();
+ reverse();
+ prevent(); }
+
+
+
+void navigate(string dir,
+              int round=1){
+
+ if (round>2){
+  emergency(); round=1;
+  break;
+ }
+ turn(dir);
+ srv.write(oppDir); //opposite direction here
+ forward(); //medium speed
+
+ while(wallInFront,
+       timer(startTime,WALL_SCAN_DURATION)){
+  startTime=millis();
+  endTime=millis();
+ }
+
+
+ if(!timer(startTime,endTime)){
+  break;
+
+ else{
+  round=2; turn(oppDir); srv(dir);
+  forward(MAX_SPEED,
+          MIN_SPEED*(endTime-startTime))
+  navigate(oppDir);
+ }
+}
+
+
+bool delayWithIR(int duration){
+ int startTime=millis();
+ while(timerRunning(startTume,duration)){
+  if (!irClear()){
+   halt();
+}
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
